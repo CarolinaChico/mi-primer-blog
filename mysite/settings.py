8002d83c1d95399500c8dcc5af9f1fe6a063a900
Carolina <carolina.chico@gmail.com>
2017-02-12 14:27:35 -0600

    permitir URL

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ocyxercmgh-5sa7@u2j12t3%b&k2xnlj$6=y6uog#@7a!-=0fj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carol24.pythonanywhere.com']
 
 
 # Application definition
